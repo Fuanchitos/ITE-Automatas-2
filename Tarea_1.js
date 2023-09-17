b0c78b81c0ba822dad5914a5f19af285d6f8d15d
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2023-09-16 22:33:06 -0700

    Instrucciones

---
+++ Tarea_1.js
@@ -0,0 +1,18 @@
+/*El algoritmo debe leer los pasos y aplicarlos a los indices del camino, ejemplo:
+
+- Si al llegar al final del arreglo aun quedan pasos, el algoritmo debera ser de arreglo circular
+- Los pasos solo deberan ser positivos.
+- Los pasos no pueden ser mayor que el camino
+--------------------------------------------
+paso[0] => 2
+camino =
+paso 0 = [1,2,3,4,5]
+paso 1 = [2,1,3,4,5]
+paso 2 = [2,3,1,4,5]
+paso[1] => 4
+camino =
+paso 0 = [1,2,3,4,5]
+paso 1 = [2,1,3,4,5]
+paso 2 = [2,3,1,4,5]
+--------------------------------------------*/
+
