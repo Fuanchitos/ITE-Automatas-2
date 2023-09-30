6d36e2cb2d7155b3b885c52b55c31231582557a9
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2023-09-29 18:38:27 -0700

    El del profe 100%

--- Apunte_28_09_2023.js
+++ Apunte_28_09_2023.js
@@ -1,33 +1,36 @@
 const fs = require('fs');
 
-function validarArray(array) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] !== '0' && array[i] !== '1') {
-            console.log("Errorsito: " + array[i]);
-            return false;
-        }
+function validaArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== '0' && array[i] !== '1') {
+      console.log("Errorsito : " + array[i]);
+      return false;
     }
-    return true; // Debe ser true si el array pasa la validación
+  }
+  return true;
 }
 
 const matrix = [];
 fs.readFile('matrix.txt', 'utf8', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
+  if (err) {
+    console.error(err);
+    return;
+  }
+  var cadena_split = data.split('\n');
+  var validar_length = cadena_split[0].length;
+  for(let i=0; i < cadena_split.length -1; i++){
+    if(validar_length == cadena_split[i].length){
+     
+      if(validaArray(cadena_split[i])){
+        matrix[i] = cadena_split[i];
+        console.log(cadena_split[i]);
+       // console.log("Contiene otros valores no permitidos");
+      }else {console.log("bad char")}
+    }else{
+      console.log("ERROR no miden lo mismo el renglon " + i);
+      break;
     }
-    var cadena_split = data.split('\n');
-    var validar_length = cadena_split[0].length;
-    for (let i = 0; i < cadena_split.length - 1; i++) { 
-        if (validar_length == cadena_split[i].length) {
-            if (validarArray(cadena_split[i])) {
-                matrix[i] = cadena_split[i];
-                console.log(cadena_split[i]);
-            } else {
-                console.log("Error en el renglón " + i);
-            }
-        } else {
-            console.log("Error: Las longitudes de las filas no coinciden en el renglón " + i);
-        }
-    }
+
+  }
+console.log(matrix);
 });
