9c405059d8babab636560cd81c1179f2cdad86d2
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2023-09-29 19:04:56 -0700

    Creacion de matrix y variables

--- Juego_Gato.js
+++ Juego_Gato.js
@@ -1,3 +1,11 @@
 const fs = require('fs');
 const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+const movimientos = 5;
+let turno = 'X';
+const movimiento_X = [[0, 0], [1, 1], [2, 2], [0, 1], [1, 0]];
+const movimiento_O = [[0, 2], [2, 0], [2, 1], [1, 2], [0, 2]];
