2dec8e24f7751d6dac79faad8087ed5d0b2925dd
Juan Carlos Salazar Silva <salazarsilvajc@gmail.com>
2023-09-29 19:06:43 -0700

    Cargar tablero

--- Juego_Gato.js
+++ Juego_Gato.js
@@ -9,3 +9,17 @@ const movimientos = 5;
 let turno = 'X';
 const movimiento_X = [[0, 0], [1, 1], [2, 2], [0, 1], [1, 0]];
 const movimiento_O = [[0, 2], [2, 0], [2, 1], [1, 2], [0, 2]];
+
+function cargarTablero(callback) {
+  fs.readFile('matrix.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.log("El archivo 'matrix.txt' no se encontró. Creando un nuevo tablero.");
+      const tablero = [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']];
+      callback(tablero);
+    } else {
+      const lineas = data.split('\n');
+      const tablero = lineas.map(linea => linea.split(' | '));
+      callback(tablero);
+    }
+  });
+}
